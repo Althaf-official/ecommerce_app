b3c418b3ea6ee297a0f0e908642b7db34194ade8
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-11 19:16:13 +0400

    created Contexdt for product detail view

--- products/views.py
+++ products/views.py
@@ -5,5 +5,9 @@ from .models import Product
 
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
+    context = {
+        "title": obj.title,
+        "description": obj.description,
+    }
 
     return render(request, "product/detail.html",{})
