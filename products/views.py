e132e4a47ef7bf1ef422250e0fa66dd23788e5f5
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-11 20:01:21 +0400

    associated with the key 'object' in the context dictionary. When you access context['object'], it returns the value

--- products/views.py
+++ products/views.py
@@ -5,9 +5,12 @@ from .models import Product
 
 def product_detail_view(request):
     obj = Product.objects.get(id=6)
+    # context = {
+    #     "title": obj.title,
+    #     "description": obj.description,
+    # }
     context = {
-        "title": obj.title,
-        "description": obj.description,
+        'object': obj#!associated with the key 'object' in the context dictionary. When you access context['object'], it returns the value
     }
 
     return render(request, "product/detail.html", context)
