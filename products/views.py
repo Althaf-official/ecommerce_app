6a082b808e7386b12838348b845e5b7da2f8b089
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-15 10:49:04 +0400

    post method csrf verification failed

--- products/views.py
+++ products/views.py
@@ -6,17 +6,21 @@ from .models import Product
 # Create your views here.
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()#!After successfully saving the form data, a new, empty instance of ProductForm is created. This step is likely intended to clear the form for a new input after successful submission. It ensures that the form fields are empty and ready to receive new data for the next submission.
+    context = {}
+    return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()#!After successfully saving the form data, a new, empty instance of ProductForm is created. This step is likely intended to clear the form for a new input after successful submission. It ensures that the form fields are empty and ready to receive new data for the next submission.
         
     
-    context = {
-        'form': form,
-    }
+#     context = {
+#         'form': form,
+#     }
 
-    return render(request, "products/product_create.html", context)
+#     return render(request, "products/product_create.html", context)
 
 
 
