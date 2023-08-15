1b78d718b822773b9ceafb228391d4c619ee257f
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-15 12:40:59 +0400

    django form overriding

--- products/forms.py
+++ products/forms.py
@@ -4,6 +4,26 @@ from django import forms
 from .models import Product
 
 class ProductForm(forms.ModelForm):
+
+    title          = forms.CharField(
+        label='',
+        # required=True is default
+        widget=forms.TextInput(
+            attrs={
+                "placeholder":"your title"
+                }))
+    description    = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "placeholder":"your Description",
+                "class": "new-class-name two",
+                "rows": "20",
+                "cols": "50",
+                "id": "my-sample-id",
+            }
+        ))
+    price          = forms.DecimalField(initial=199.99)
     class Meta:
         model = Product
         fields = [
@@ -11,6 +31,8 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+        
 
 class RawProductForm(forms.Form):
     title          = forms.CharField(
--- products/views.py
+++ products/views.py
@@ -5,20 +5,20 @@ from .forms import ProductForm,RawProductForm
 from .models import Product
 # Create your views here.
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == "POST":
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)#!By using **, you're unpacking the dictionary's contents and passing them as keyword arguments to the create() method.
-            my_form=RawProductForm()
-        else:
-            print(my_form.errors)
-    context = {
-        "form": my_form,
-    }
-    return render(request, "products/product_create.html", context)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == "POST":
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)#!By using **, you're unpacking the dictionary's contents and passing them as keyword arguments to the create() method.
+#             my_form=RawProductForm()
+#         else:
+#             print(my_form.errors)
+#     context = {
+#         "form": my_form,
+#     }
+#     return render(request, "products/product_create.html", context)
 
 # def product_create_view(request):
 #     #print(request.GET)
@@ -30,18 +30,18 @@ def product_create_view(request):
 #     context = {}
 #     return render(request, "products/product_create.html", context)
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()#!After successfully saving the form data, a new, empty instance of ProductForm is created. This step is likely intended to clear the form for a new input after successful submission. It ensures that the form fields are empty and ready to receive new data for the next submission.
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()#!After successfully saving the form data, a new, empty instance of ProductForm is created. This step is likely intended to clear the form for a new input after successful submission. It ensures that the form fields are empty and ready to receive new data for the next submission.
         
     
-#     context = {
-#         'form': form,
-#     }
+    context = {
+        'form': form,
+    }
 
-#     return render(request, "products/product_create.html", context)
+    return render(request, "products/product_create.html", context)
 
 
 
