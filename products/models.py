5abd614426f59a5bace23c72355b6b4b0745b3be
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-19 18:24:17 +0400

    dynamic url when you call get_absolute_url() on an instance of the class where this method is defined, it will return a URL string that represents the absolute URL for that instance. For example, if you have a product with an id of 42, calling product.get_absolute_url() would return "/products/42/", which could be used to construct a link to that specific product's detail page in a Django web application.

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Product(models.Model):
@@ -7,3 +8,8 @@ class Product(models.Model):
     price       = models.DecimalField(max_digits=100000,decimal_places=2)
     summary     = models.TextField(blank=True,null=False)
     features    = models.BooleanField(default=False)
+
+def get_absolute_url(self):
+    return f"/products/{self.id}/"
+    #!when you call get_absolute_url() on an instance of the class where this method is defined, it will return a URL string that represents the absolute URL for that instance. For example, if you have a product with an id of 42, calling product.get_absolute_url() would return "/products/42/", which could be used to construct a link to that specific product's detail page in a Django web application.
+    
