74937959362ec62742ca4eaa5b2a8a68afd92b9a
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-20 15:35:15 +0400

    added product detatil view

--- ecommerce_app/urls.py
+++ ecommerce_app/urls.py
@@ -23,13 +23,17 @@ from products.views import (
     dynamic_lookup_view,
     product_delete_view,
     product_list_view,
+    product_update_view,
+    
 )
 urlpatterns = [
     path("products/", product_list_view, name="product-list"),
-    path("products/<int:id>/",dynamic_lookup_view, name='product-detail'),
-    path("products/<int:id>/delete/",product_delete_view, name='product-delete'),
-    # path("create/",product_create_view),
-    # path("product/",product_detail_view),
+    path("products/create/", product_create_view, name="product"),
+    path("products/<int:id>/", product_detail_view, name='product-detail'),
+    path("products/<int:id>/update/", product_update_view, name="product-update"),
+    path("products/<int:id>/delete/", product_delete_view, name='product-delete'),
+    
+
     path('admin/', admin.site.urls),
 ]
 
--- products/views.py
+++ products/views.py
@@ -104,8 +104,19 @@ def product_create_view(request):
 
 
 
-def product_detail_view(request):
-    obj = Product.objects.get(id=6)
+def product_update_view(request,id=id):
+    obj = get_object_or_404(Product, id=id)
+    form = ProductForm(request.POST or None,instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {
+        "form": form
+    }
+    return render(request,"products/product_create.html", context)
+
+
+def product_detail_view(request,id=id):
+    obj = Product.objects.get(id=id)
     # context = {
     #     "title": obj.title,
     #     "description": obj.description,
