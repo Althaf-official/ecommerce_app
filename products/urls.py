8a11cedc0f1ab389e70dd3ed87cec3a4250ba0ef
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-21 16:50:26 +0400

    product url views imported views

---
+++ products/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .views import( 
+    product_create_view,
+    product_update_view, 
+    product_delete_view, 
+    product_detail_view,
+    product_list_view,
+)
+
