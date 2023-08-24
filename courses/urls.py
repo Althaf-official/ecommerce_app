2da299b3017b65097b9328b77177e17f2e062f14
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-24 17:44:06 +0400

    showed empty templete

--- courses/urls.py
+++ courses/urls.py
@@ -12,4 +12,5 @@ app_name = "courses"
 urlpatterns = [
     #path("", my_fbv, name="courses-list"),
     path("", CourseView.as_view(template_name="contact.html"), name="courses-list"),
+    path("<int:id>", CourseView.as_view(), name="courses-detail"),
 ]
