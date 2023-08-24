97829fbfacd9fd15cd75e3b60d41799327704936
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-24 17:05:54 +0400

    this is function base view example

--- courses/views.py
+++ courses/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def my_fbv(request, *args, **kwargs):
     return render(request, "about.html",{})
+    # this is function base view example
