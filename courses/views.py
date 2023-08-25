ce563dacb8c204f18f0d0ece843b9630cdfd58ae
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-25 15:59:04 +0400

    CourseListView class is designed to handle GET requests and render a list of courses using a specified HTML template. The course data is retrieved from the database using a queryset defined in the queryset attribute, and this data is passed to the template for rendering

--- courses/views.py
+++ courses/views.py
@@ -5,6 +5,14 @@ from django.views import View
 from .models import Course
 
 
+class CourseListView(View):
+    template_name = "courses/course_list.html"
+    queryset = Course.objects.all()#!This queryset will be used later to fetch data to display in the template
+    
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name,{'object_list':self.queryset})
+
 class CourseView(View):
     template_name = "courses/course_detail.html"
     def get(self,request, id=None ,*args,**kwargs):
