f155fd64d87eb79652422a8baf31845af98fc263
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-27 23:39:42 +0400

    succesfully showed  courses list view

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from .views import (
     my_fbv,
-    CourseView
+    CourseView,
+    CourseListView,
     )
 
 
@@ -11,6 +12,7 @@ app_name = "courses"
 
 urlpatterns = [
     #path("", my_fbv, name="courses-list"),
+    path("", CourseListView.as_view(), name="courses-list"),
     path("", CourseView.as_view(template_name="contact.html"), name="courses-list"),
     path("<int:id>", CourseView.as_view(), name="courses-detail"),
 ]
--- courses/views.py
+++ courses/views.py
@@ -9,8 +9,12 @@ class CourseListView(View):
     template_name = "courses/course_list.html"
     queryset = Course.objects.all()#!This queryset will be used later to fetch data to display in the template
 
+    def get_queryset(self):
+        return self.queryset
+
 
     def get(self, request, *args, **kwargs):
+        context = {'object_list': self.get_queryset()}
         return render(request, self.template_name,{'object_list':self.queryset})
         #!eturn render(request, self.template_name, {'object_list': self.queryset}): This line returns an HTTP response using the render function. Here's what each part of this line does:
 
